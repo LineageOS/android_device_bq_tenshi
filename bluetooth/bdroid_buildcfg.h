90b235bec36616ec8dacd665fdba1c2a2e287fa1
Niraj kumar Mishra <nmishr@codeaurora.org>
2017-02-02 19:00:19 +0530

    BT L2CAP RFCOMM: Increase max no of l2cap channels(2/2)
    To support more connections,max L2cap channels increased
    from 16 to 20
    
    Change-Id: Ic79435b0cef954cc61816243db486201302c8a8e
    CRs-Fixed: 2001922

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 #define BTM_DEF_LOCAL_NAME   "Aquaris U Plus"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    20
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
