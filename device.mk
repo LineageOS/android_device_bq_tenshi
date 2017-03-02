ca3764d2744539006672307b6d3dfc1b1cd83fff
Brinly Taylor <brinly@brinly.me>
2017-03-03 08:33:27 +1100

    tenshi: Don't extract configs, just ship configs
    * Less pain, more gain.
    
    * BQ package version: 1.3.2_20161216-1400
    
    Change-Id: If5d5fc6f5704d003c995ac368c64766a2fa8cbd0

--- device.mk
+++ device.mk
@@ -89,6 +89,11 @@ PRODUCT_PACKAGES += \
     memtrack.msm8937 \
     liboverlay
 
+# Data
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
 # Fingerprint
 PRODUCT_PACKAGES += \
