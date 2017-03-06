1b145a4e13205caf360b673a90bbce8c2d00548f
Brinly T <brinly@brinly.me>
2017-03-06 15:27:33 +1100

    tenshi: use fingerprintd prebuilt
    * I don't have time to reverse engineer the differences
    * so this will have to do for now.
    
    Change-Id: I9469434c284167110837466b08a733b6a3c930f2

--- device.mk
+++ device.mk
@@ -95,10 +95,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd
-
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
