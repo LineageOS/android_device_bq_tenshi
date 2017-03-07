41d9f2d0baa83bca754f89ad55742d988197619f
Eloi Carracedo <eloimuns@gmail.com>
2017-03-07 19:21:59 +0100

    tenshi: Build OSS GPS HAL
    * Don't include some older proprietary files too.
    
    Change-Id: I6e1f338081846c780809d0b91084a94052debec8

--- device.mk
+++ device.mk
@@ -95,6 +95,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8937 \
+    gps.conf
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
