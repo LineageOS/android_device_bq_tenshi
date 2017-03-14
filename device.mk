65a5e7cc1406c54d709592fd0bcddcbbbad60e29
Brinly T <brinly@brinly.me>
2017-03-14 14:49:54 +1100

    tenshi: Build Internet Packet Accelerator module
    Change-Id: I456db87f6163fef7cc4b6937dd65282b1a72054c

--- device.mk
+++ device.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# IPA Manager
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
