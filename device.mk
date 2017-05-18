5c033873f7132a06b62053219fd44b927b10c27d
Anik1199 <anik@lineageos.org>
2017-05-19 03:06:50 +0600

    tenshi: Always include vendor makefiles
    Change-Id: I78370516dc62b016e710fba991c009680ee2c7df

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/bq/tenshi/tenshi-vendor.mk)
+$(call inherit-product, vendor/bq/tenshi/tenshi-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
