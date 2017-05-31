95eec15841ed13ea423706198ff9395d992df772
Brinly T <brinly@brinly.me>
2017-05-31 13:17:59 +1000

    tenshi: experiments do not merge.
    Change-Id: I02a73bac8f07f66c3f68f96077566645874b34ea

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,11 +32,15 @@ PRODUCT_BRAND := bq
 PRODUCT_MODEL := Aquaris U Plus
 PRODUCT_MANUFACTURER := bq
 
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=tenshi
+
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="bq/tenshi/tenshi:7.1.1/NMF26F/1492687806:user/release-keys" \
+    BUILD_FINGERPRINT=bq/tenshi/tenshi:7.1.1/NMF26F/1492687806:user/release-keys \
     PRIVATE_BUILD_DESC="tenshi-user 7.1.1 NMF26F 2.1.0_20170420-1318 release-keys"
