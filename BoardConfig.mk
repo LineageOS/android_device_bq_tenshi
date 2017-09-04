19888b103464d457f6f2e02ab0bd29e07093e3af
Eloi Carracedo <eloimuns@gmail.com>
2017-09-04 20:51:54 +0200

    tenshi: Rework tenshi wifi
    Change-Id: I65f687f57beb730d92b8749d6b537cf0fa47529e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_PAGESIZE := 2048
