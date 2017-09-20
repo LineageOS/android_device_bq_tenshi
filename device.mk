b123979b9aa5ededbd0f5474817d1f9f21dc1d52
Eloi Carracedo <eloimuns@gmail.com>
2017-09-20 17:09:07 +0200

    tenshi: Rework ramdisk
    Change-Id: I28114c4d903b774a24ad131be70e68204c45e5d2

--- device.mk
+++ device.mk
@@ -147,12 +147,12 @@ PRODUCT_BOOT_JARS += \
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.qcom.bt.sh \
     init.qcom.early_boot.sh \
+		init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.power.rc \
     init.qcom.usb.rc \
+		init.target.rc \
     ueventd.qcom.rc
 
 # IPA Manager
