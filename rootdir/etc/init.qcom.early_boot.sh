94ce2c3f2d4a4531e061eee85245bbf8874ca6a4
Eloi Carracedo <eloimuns@gmail.com>
2017-09-26 23:06:13 +0200

    tenshi: rootdir: Remove HDMI Permissions
    Change-Id: I1f642b3b0e3f5cee64b6c82e24bd40e7d288cc53

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -83,48 +83,6 @@ function set_perms() {
     chmod $3 $1
 }
 
-function setHDMIPermission() {
-   file=/sys/class/graphics/fb$1
-   dev_file=/dev/graphics/fb$1
-   dev_gfx_hdmi=/devices/virtual/switch/hdmi
-
-   set_perms $file/hpd system.graphics 0664
-   set_perms $file/res_info system.graphics 0664
-   set_perms $file/vendor_name system.graphics 0664
-   set_perms $file/product_description system.graphics 0664
-   set_perms $file/video_mode system.graphics 0664
-   set_perms $file/format_3d system.graphics 0664
-   set_perms $file/s3d_mode system.graphics 0664
-   set_perms $file/cec/enable system.graphics 0664
-   set_perms $file/cec/logical_addr system.graphics 0664
-   set_perms $file/cec/rd_msg system.graphics 0664
-   set_perms $file/pa system.graphics 0664
-   set_perms $file/cec/wr_msg system.graphics 0600
-   set_perms $file/hdcp/tp system.graphics 0664
-   set_perms $file/hdmi_audio_cb audioserver.audio 0600
-   ln -s $dev_file $dev_gfx_hdmi
-}
-
-# check for HDMI connection
-for fb_cnt in 0 1 2
-do
-    file=/sys/class/graphics/fb$fb_cnt/msm_fb_panel_info
-    if [ -f "$file" ]
-    then
-      cat $file | while read line; do
-        case "$line" in
-            *"is_pluggable"*)
-             case "$line" in
-                  *"1"*)
-                  setHDMIPermission $fb_cnt
-             esac
-        esac
-      done
-    fi
-done
-
-
-
 # check for mdp caps
 setprop debug.gralloc.gfx_ubwc_disable 1
 file=/sys/class/graphics/fb0/mdp/caps
