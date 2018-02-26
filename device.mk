e66afdc6c0e225d30f1ac0e55fa54140434d93f8
mifl <mifl.pc@gmail.com>
2018-02-26 20:13:11 +0100

    tenshi: clean unused camera configs
    Change-Id: Ibea16cc328c36fc807954c7316a5331d78b32476
    Signed-off-by: mifl <mifl.pc@gmail.com>

--- device.mk
+++ device.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/camera_config.xml:system/etc/camera/camera_config.xml \
-    $(LOCAL_PATH)/configs/camera/ov5670_f5670bq_chromatix.xml:system/etc/camera/ov5670_f5670bq_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/ov5675_d5v15b_chromatix.xml:system/etc/camera/ov5675_d5v15b_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/s5k3p3sm_chromatix.xml:system/etc/camera/s5k3p3sm_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/s5k3p3_chromatix.xml:system/etc/camera/s5k3p3_chromatix.xml
